8890e36381911d707f87dbe4c1560366087b9c3c
JaeYoonAI <minamljy@gmail.com>
2023-09-22 00:43:55 +0900

    drf settings

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,51 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from articles.models import Article
+from articles.serializers import ArticleSerializer
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from braces.views import CsrfExemptMixin
+from drf_yasg.utils import swagger_auto_schema
 
 
 # Create your views here.
-@api_view(["GET"])
-def index(request):
-    return Response("연결되었습니다!")
+
+
+class ArticleList(APIView):
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(request_body=ArticleSerializer)
+    def post(self, request, format=None):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetail(APIView):
+    def get(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
